9254e836529a7326bc667cf16b4c42cb83c87808
gitThi254 <138993372+gitThi254@users.noreply.github.com>
2025-01-02 00:38:09 +0700

    added dockerfile v2

--- src/main/java/com/example/deploy_v1/TestController.java
+++ src/main/java/com/example/deploy_v1/TestController.java
@@ -1,12 +1,16 @@
 package com.example.deploy_v1;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    @Value("${api.test}")
+    private String test;
     @GetMapping("")
     public String hello() {
-        return "hello world";
+        return "TEST: " + test;
     }
 }
